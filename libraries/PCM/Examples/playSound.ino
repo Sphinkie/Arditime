6c0393de2f7dfd76c188cc8a5129b9cc864f0ce0
David de Lorenzo <de.lorenzo.david@gmail.com>
2018-01-24 08:38:37 +0100

    external libraries used by the project
    Arduino PCM library
    Arduino SimpleTimer library

---
+++ libraries/PCM/Examples/playSound.ino
@@ -0,0 +1,21 @@
+
+// SpeakerPin = 11 
+
+#include "PCM.h"
+// #include "PCM_Sound.h"
+#include "Gonghi.h"
+
+void setup()
+{
+  // startPlayback(gonghi_PCM, gonghi_Len); // gros gresillement
+  startPlayback(data, NUM_ELEMENTS); 
+  
+}
+
+void loop()
+{
+	delay (5000);
+   startPlayback(data, NUM_ELEMENTS); 
+}
+
+
